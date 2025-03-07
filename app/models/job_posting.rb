47dee5b184179ca9c5965feba80d9a9a75ac2f79
Vinícius <business.fernandes22@gmail.com>
2025-03-07 14:31:09 -0300

    Job posting Model tests

--- app/models/job_posting.rb
+++ app/models/job_posting.rb
@@ -1,4 +1,6 @@
 class JobPosting < ApplicationRecord
   belongs_to :company_profile
   belongs_to :job_type
+
+  validates :title, :salary, :salary_currency, :salary_period, presence: true
 end
--- spec/models/job_posting_spec.rb
+++ spec/models/job_posting_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe JobPosting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "#Valid?" do
+    it "fails when title is nil" do
+      job_posting = build(:job_posting, title: nil)
+      job_posting.valid?
+      expect(job_posting).not_to be_valid
+    end
+
+    it "fails when salary is nil" do
+      job_posting = build(:job_posting, salary: nil)
+      job_posting.valid?
+      expect(job_posting).not_to be_valid
+    end
+
+    it "fails when salary_currency is nil" do
+      job_posting = build(:job_posting, salary_currency: nil)
+      job_posting.valid?
+      expect(job_posting).not_to be_valid
+    end
+
+    it "fails when salary_period is nil" do
+      job_posting = build(:job_posting, salary_period: nil)
+      job_posting.valid?
+      expect(job_posting).not_to be_valid
+    end
+  end
 end
