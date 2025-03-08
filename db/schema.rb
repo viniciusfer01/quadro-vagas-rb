ba30d58163891941e0209141ca41900fffd4dd9d
Willian Daniel <williandeivitidaniel@live.com>
2025-03-07 22:44:44 -0300

    feat: Add user attributes and validations for name, last name, and email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_06_231757) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_07_224801) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -60,6 +60,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_06_231757) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
+    t.string "name"
+    t.string "last_name"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
