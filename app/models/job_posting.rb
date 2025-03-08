9a615d4aa30a8c108fd15c2ecb04176f61b87e2b
Matheus Santana <matheusfsantana99@gmail.com>
2025-03-08 13:26:47 -0300

    Search jobs by title and description with trigam word similarity search
    Co-authored-by: Bruno Herculano <herculanoterto@gmail.com>

--- app/models/job_posting.rb
+++ app/models/job_posting.rb
@@ -1,13 +1,12 @@
 class JobPosting < ApplicationRecord
   include PgSearch::Model
   pg_search_scope :search_jobs,
-    against:  :title,
-    using: {
-      tsearch: {
-        any_word: true,
-        prefix: true
-      }
-    }
+                  against:  [ :title, :description ],
+                  using: {
+                    trigram: {
+                      word_similarity: true
+                    }
+                  }
 
   belongs_to :company_profile
   has_one :job_type
--- spec/system/visitor_search_jobs_spec.rb
+++ spec/system/visitor_search_jobs_spec.rb
@@ -1,37 +1,37 @@
 require 'rails_helper'
 
-describe "Visitor search for jobs", type: :system do
+describe "Visitor search for jobs by title and description", type: :system do
   it 'successfuly' do
-    create(:job_posting, title: "RUBY on Rails Dev Jr.")
-    create(:job_posting, title: "Sinatra Developer Jr.")
-    create(:job_posting, title: "Sinatra Dev Pl.")
-    create(:job_posting, title: "Rails Developer Pl.")
-    create(:job_posting, title: "DragonRuby Game Dev Sr.")
+    create(:job_posting, title: "RUBY on Rails Dev Jr.", description: "Rails dev Jr")
+    create(:job_posting, title: "Sinatra Developer Jr.", description: "Ruby developer")
+    create(:job_posting, title: "Sinatra Dev Pl.", description: "Clean Architecture")
+    create(:job_posting, title: "Rails Developer Pl.", description: "TDD e RSpec")
+    create(:job_posting, title: "DragonRuby Game Dev Sr.", description: "Game Developer")
 
     visit root_path
     fill_in "title", with: "ruby"
     find("#search_button").click
 
     expect(page).to have_content "RUBY on Rails Dev Jr."
-    expect(page).not_to have_content "DragonRuby Game Dev Sr."
-    expect(page).not_to have_content "Sinatra Developer Jr."
+    expect(page).to have_content "Sinatra Developer Jr."
+    expect(page).to have_content "DragonRuby Game Dev Sr."
     expect(page).not_to have_content "Sinatra Dev Pl."
     expect(page).not_to have_content "Rails Developer Pl."
   end
 
   it 'and search empty' do
-    create(:job_posting, title: "Ruby on Rails Dev Jr.")
-    create(:job_posting, title: "Sinatra Developer Jr.")
-    create(:job_posting, title: "Sinatra Dev Pl.")
-    create(:job_posting, title: "Rails Developer Pl.")
-    create(:job_posting, title: "DragonRuby Game Dev Sr.")
+    create(:job_posting, title: "RUBY on Rails Dev Jr.", description: "Rails dev Jr")
+    create(:job_posting, title: "Sinatra Developer Jr.", description: "Ruby developer")
+    create(:job_posting, title: "Sinatra Dev Pl.", description: "Clean Architecture")
+    create(:job_posting, title: "Rails Developer Pl.", description: "TDD e RSpec")
+    create(:job_posting, title: "DragonRuby Game Dev Sr.", description: "Game Developer")
 
     visit root_path
     fill_in "title", with: ""
     find("#search_button").click
 
     expect(page).to have_content "Por favor, digite algum valor para a busca"
-    expect(page).to have_content "Ruby on Rails Dev Jr."
+    expect(page).to have_content "RUBY on Rails Dev Jr."
     expect(page).to have_content "DragonRuby Game Dev Sr."
     expect(page).to have_content "Sinatra Developer Jr."
     expect(page).to have_content "Sinatra Dev Pl."
@@ -39,11 +39,11 @@ describe "Visitor search for jobs", type: :system do
   end
 
   it 'and not found jobs' do
-    create(:job_posting, title: "Ruby on Rails Dev Jr.")
-    create(:job_posting, title: "Sinatra Developer Jr.")
-    create(:job_posting, title: "Sinatra Dev Pl.")
-    create(:job_posting, title: "Rails Developer Pl.")
-    create(:job_posting, title: "DragonRuby Game Dev Sr.")
+    create(:job_posting, title: "RUBY on Rails Dev Jr.", description: "Rails dev Jr")
+    create(:job_posting, title: "Sinatra Developer Jr.", description: "Ruby developer")
+    create(:job_posting, title: "Sinatra Dev Pl.", description: "Clean Architecture")
+    create(:job_posting, title: "Rails Developer Pl.", description: "TDD e RSpec")
+    create(:job_posting, title: "DragonRuby Game Dev Sr.", description: "Game Developer")
 
     visit root_path
     fill_in "title", with: "Node"
