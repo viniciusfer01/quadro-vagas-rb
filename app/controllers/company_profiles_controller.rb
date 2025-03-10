e9f2d676f5c0dd348819a63a1567c317da2215af
Rodrigo Moreno <rmoreno.w@gmail.com>
2025-03-10 14:19:19 -0300

    test: Adds request tests

--- app/controllers/company_profiles_controller.rb
+++ app/controllers/company_profiles_controller.rb
@@ -4,10 +4,16 @@ class CompanyProfilesController < ApplicationController
   end
 
   def new
+    redirect_to_root_if_user_already_has_a_company_profile
+    return if performed?
+
     @company_profile = CompanyProfile.new
   end
 
   def create
+    redirect_to_root_if_user_already_has_a_company_profile
+    return if performed?
+
     @company_profile = Current.user.build_company_profile(company_profile_params)
 
     if @company_profile.save
@@ -19,6 +25,9 @@ class CompanyProfilesController < ApplicationController
   end
 
   private
+  def redirect_to_root_if_user_already_has_a_company_profile
+    redirect_to root_path, alert: t(".user_already_has_a_company_profile") if Current.user.company_profile.present?
+  end
 
   def company_profile_params
     params.require(:company_profile).permit(:name, :website_url, :contact_email, :logo)
---
+++ spec/requests/company_profiles/user_creates_company_profile_request_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+describe 'User tries to create a company profile' do
+  it 'and fails to get the form for not being authenticated', type: :request do
+    get(new_company_profile_path)
+
+    expect(response).to redirect_to new_session_path
+    expect(response.status).to eq 302
+  end
+
+  it 'and fails to get the form because they already have a company profile', type: :request do
+    user = FactoryBot.create(:user)
+    FactoryBot.create(:company_profile, user: user)
+    cookie_jar = ActionDispatch::Request.new(Rails.application.env_config.deep_dup).cookie_jar
+    user.sessions.create!.tap do |session|
+      cookie_jar.signed.permanent[:session_id] = { value: session.id, httponly: true, samesite: :lax }
+      cookies[:session_id] = cookie_jar[:session_id]
+    end
+
+    get(new_company_profile_path)
+
+    expect(response).to redirect_to root_path
+    expect(response.status).to eq 302
+  end
+
+  it 'and fails to create a company profile for being unauthenticated', type: :request do
+    user = FactoryBot.create(:user)
+    FactoryBot.create(:company_profile, user: user)
+    file = Rack::Test::UploadedFile.new(Rails.root.join('spec', 'support', 'files', 'logo.jpg'), 'image/jpg')
+
+    post(company_profiles_path, params: { company_profile: {
+      name: 'Company',
+      contact_email: 'contact@company.com',
+      website_url: 'https://company.com',
+      logo: file
+    } })
+
+    expect(response).to redirect_to new_session_path
+    expect(response.status).to eq 302
+  end
+
+  it 'and fails to create a company profile because they already have one', type: :request do
+    user = FactoryBot.create(:user)
+    FactoryBot.create(:company_profile, user: user)
+    cookie_jar = ActionDispatch::Request.new(Rails.application.env_config.deep_dup).cookie_jar
+    user.sessions.create!.tap do |session|
+      cookie_jar.signed.permanent[:session_id] = { value: session.id, httponly: true, samesite: :lax }
+      cookies[:session_id] = cookie_jar[:session_id]
+    end
+    file = Rack::Test::UploadedFile.new(Rails.root.join('spec', 'support', 'files', 'logo.jpg'), 'image/jpg')
+
+    post(company_profiles_path, params: { company_profile: {
+      name: 'Company',
+      contact_email: 'contact@company.com',
+      website_url: 'https://company.com',
+      logo: file
+    } })
+
+    expect(response).to redirect_to root_path
+    expect(response.status).to eq 302
+  end
+end
