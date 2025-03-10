512b215747390934e7885b5b125925066ac4b109
Vinícius <business.fernandes22@gmail.com>
2025-03-10 12:32:58 -0300

    fixes system tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Delete all records from the database
+CompanyProfile.delete_all
+JobPosting.delete_all
+JobType.delete_all
 
 # Creates three job types
 [ "Remote", "Hybrid", "In-Office" ].each do |job_type_name|
-  JobType.find_or_create_by!(name: job_type_name)
+  JobType.create!(name: job_type_name)
 end
 
 # Creates three company profiles
 3.times do |n|
-  CompanyProfile.find_or_create_by!(name: "Company Name #{n}", website_url: "http://company#{n}.com", contact_email: "contact@company#{n}.com")
+  CompanyProfile.create!(name: "Company Name #{n}", website_url: "http://company#{n}.com", contact_email: "contact@company#{n}.com")
 end
 
 # Creates three job postings
-JobPosting.find_or_create_by!(title: "Dev React", company_profile: CompanyProfile.first, salary: "1000.00", salary_currency: "USD", salary_period: "Monthly", job_type: JobType.first)
-JobPosting.find_or_create_by!(title: "Dev Node", company_profile: CompanyProfile.second, salary: "5000.00", salary_currency: "USD", salary_period: "Monthly", job_type: JobType.second)
-JobPosting.find_or_create_by!(title: "Dev RoR", company_profile: CompanyProfile.last, salary: "10000.00", salary_currency: "USD", salary_period: "Monthly", job_type: JobType.last)
+JobPosting.create!(title: "Dev React", company_profile: CompanyProfile.first, salary: "1000.00", salary_currency: "USD", salary_period: "Monthly", job_type: JobType.first, description: "Software Developer")
+JobPosting.create!(title: "Dev Node", company_profile: CompanyProfile.second, salary: "5000.00", salary_currency: "USD", salary_period: "Monthly", job_type: JobType.second, description: "Software Developer")
+JobPosting.create!(title: "Dev RoR", company_profile: CompanyProfile.last, salary: "10000.00", salary_currency: "USD", salary_period: "Monthly", job_type: JobType.last, description: "Software Developer")
