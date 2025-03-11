7acee0d1252660c03a1bc8be9bc54ed2d05a9f5d
gctoledo <gabrielcostatoledo@outlook.com>
2025-03-11 19:08:11 -0300

    fix: ensures inactive job_postings cant be access

--- app/controllers/job_postings_controller.rb
+++ app/controllers/job_postings_controller.rb
@@ -1,7 +1,17 @@
 class JobPostingsController < ApplicationController
   allow_unauthenticated_access
+  before_action :set_job_posting, only: %i[ show ]
+  before_action :check_inactive_job_posting, only: %i[ show ]
 
-  def show
-    @job_posting = JobPosting.find(params[:id])
+  def show; end
+
+  private
+
+  def set_job_posting
+    @job_posting = JobPosting.unscoped.find(params[:id])
+  end
+
+  def check_inactive_job_posting
+    redirect_to root_path if @job_posting.inactive?
   end
 end
