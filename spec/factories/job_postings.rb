dc722f32859deaec5438c0db7d0d8b007ae0ba23
gctoledo <gabrielcostatoledo@outlook.com>
2025-03-14 17:15:06 -0300

    test: remove possible duplicate creatin on JobPosting factory

--- spec/factories/job_postings.rb
+++ spec/factories/job_postings.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
   factory :job_posting do
     sequence(:title) { |n| "Job Title #{n}" }
-    company_profile { create(:company_profile) }
     sequence(:salary) { |n| "Salary #{n}" }
     salary_currency { :usd }
     salary_period { :monthly }
     work_arrangement { :remote }
-    job_type { create(:job_type) }
     job_location { "City, Country" }
     description { "Something" }
 
     association :experience_level
+    association :job_type
+    association :company_profile
   end
 end
