ab48706927f9e04e1ac4bd711325f7081acb78d9
gctoledo <gabrielcostatoledo@outlook.com>
2025-03-14 00:59:39 -0300

    feat: add helper methods to admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+
+  def only_admin_access
+    redirect_to root_path, alert: t(".unauthorized") unless admin?
+  end
 end
