8e2341883725e405643d7dafeca8199b96c01871
gctoledo <gabrielcostatoledo@outlook.com>
2025-03-14 15:24:37 -0300

    chore: remove "status" from CompanyProfile e JobPosting tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_10_215004) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_14_164123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -50,7 +50,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_10_215004) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.integer "status", default: 0, null: false
     t.index ["user_id"], name: "index_company_profiles_on_user_id"
   end
 
@@ -70,7 +69,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_10_215004) do
     t.datetime "updated_at", null: false
     t.bigint "job_type_id", null: false
     t.text "description"
-    t.integer "status", default: 0, null: false
     t.index ["company_profile_id"], name: "index_job_postings_on_company_profile_id"
     t.index ["job_type_id"], name: "index_job_postings_on_job_type_id"
   end
